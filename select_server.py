1c2f851950619ef2749d62a532dccf0bf531d84d
Micah Stalberg <stalberm@oregonstate.edu>
2022-11-14 10:07:13 -0800

    Extract methods for handling ready sockets

--- select_server.py
+++ select_server.py
@@ -6,30 +6,34 @@ import sys
 import socket
 import select
 
+def handle_listener(ready_socket, read_set):
+    new_conn = ready_socket.accept()[0]
+    print(f"{new_conn.getpeername()}: connected")
+    read_set.add(new_conn)
+
+def handle_standard(ready_socket, read_set):
+    request = ready_socket.recv(4096)
+    print(f"{ready_socket.getpeername()} {len(request)} bytes: {request}")
+    if (len(request) == 0):
+        read_set.remove(ready_socket)
+        print(f"{ready_socket.getpeername()}: disconnected")
 
 def run_server(port):
+
     listener = socket.socket()
     listener.bind(('', int(port)))
     listener.listen()
-
     read_set = {listener}
 
     while True:
+
         read, _, _ = select.select(read_set, {}, {})
 
         for ready_socket in read:
             if ready_socket == listener:
-                new_conn = ready_socket.accept()[0]
-                print(f"{new_conn.getpeername()}: connected")
-                read_set.add(new_conn)
+                handle_listener(ready_socket, read_set)
             else:
-                request = ready_socket.recv(4096)
-                print(f"{ready_socket.getpeername()} {len(request)} bytes: {request}")
-                if (len(request) == 0):
-                    read_set.remove(ready_socket)
-                    print(f"{ready_socket.getpeername()}: disconnected")
-
-                
+                handle_standard(ready_socket, read_set)
 
 
 #--------------------------------#
